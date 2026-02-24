c8f35d229d7c5eecc5198bbb8205df129b7a2998
Okuta Joshua <okutajoshuatash256@gmail.com>
2026-02-24 03:56:43 +0300

    First Commit
    Big Wallet Ledger

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'big-wallet-v1';
+const assets = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/app.js',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap'
+];
+
+// Install Service Worker
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// Fetching assets
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(rec => {
+      return rec || fetch(evt.request);
+    })
+  );
+});
